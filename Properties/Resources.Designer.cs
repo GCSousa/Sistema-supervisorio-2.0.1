c3e4dc60aaa5e1254d6ac8b1fb41611219edb24c
Guilherne Sousa <69986440+GCSousa@users.noreply.github.com>
2025-12-17 01:02:59 -0300

    Add files via upload

---
+++ Properties/Resources.Designer.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace SupervisorioDana.Properties
+{
+	// Token: 0x02000012 RID: 18
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	internal class Resources
+	{
+		// Token: 0x060001B6 RID: 438 RVA: 0x00038716 File Offset: 0x00036916
+		internal Resources()
+		{
+		}
+
+		// Token: 0x17000001 RID: 1
+		// (get) Token: 0x060001B7 RID: 439 RVA: 0x00038720 File Offset: 0x00036920
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				bool flag = Resources.resourceMan == null;
+				if (flag)
+				{
+					ResourceManager temp = new ResourceManager("SupervisorioDana.Properties.Resources", typeof(Resources).Assembly);
+					Resources.resourceMan = temp;
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		// Token: 0x17000002 RID: 2
+		// (get) Token: 0x060001B8 RID: 440 RVA: 0x00038768 File Offset: 0x00036968
+		// (set) Token: 0x060001B9 RID: 441 RVA: 0x0003877F File Offset: 0x0003697F
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		// Token: 0x17000003 RID: 3
+		// (get) Token: 0x060001BA RID: 442 RVA: 0x00038788 File Offset: 0x00036988
+		internal static Bitmap aberto
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("aberto", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x17000004 RID: 4
+		// (get) Token: 0x060001BB RID: 443 RVA: 0x000387B8 File Offset: 0x000369B8
+		internal static Bitmap ALARME
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("ALARME", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x17000005 RID: 5
+		// (get) Token: 0x060001BC RID: 444 RVA: 0x000387E8 File Offset: 0x000369E8
+		internal static Bitmap CADASTRO
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("CADASTRO", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x17000006 RID: 6
+		// (get) Token: 0x060001BD RID: 445 RVA: 0x00038818 File Offset: 0x00036A18
+		internal static Bitmap CONFIGURAÇÕES
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("CONFIGURAÇÕES", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x17000007 RID: 7
+		// (get) Token: 0x060001BE RID: 446 RVA: 0x00038848 File Offset: 0x00036A48
+		internal static Bitmap fechado
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("fechado", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x17000008 RID: 8
+		// (get) Token: 0x060001BF RID: 447 RVA: 0x00038878 File Offset: 0x00036A78
+		internal static Bitmap fechado1
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("fechado1", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x17000009 RID: 9
+		// (get) Token: 0x060001C0 RID: 448 RVA: 0x000388A8 File Offset: 0x00036AA8
+		internal static Bitmap FUNDO
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("FUNDO", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x1700000A RID: 10
+		// (get) Token: 0x060001C1 RID: 449 RVA: 0x000388D8 File Offset: 0x00036AD8
+		internal static Bitmap Fundoo
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("Fundoo", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x1700000B RID: 11
+		// (get) Token: 0x060001C2 RID: 450 RVA: 0x00038908 File Offset: 0x00036B08
+		internal static Bitmap INICIO
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("INICIO", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x1700000C RID: 12
+		// (get) Token: 0x060001C3 RID: 451 RVA: 0x00038938 File Offset: 0x00036B38
+		internal static Bitmap LOGIN___reto
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("LOGIN - reto", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x1700000D RID: 13
+		// (get) Token: 0x060001C4 RID: 452 RVA: 0x00038968 File Offset: 0x00036B68
+		internal static Bitmap Software_Dana__Imagens_2
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("Software_Dana (Imagens)2", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x1700000E RID: 14
+		// (get) Token: 0x060001C5 RID: 453 RVA: 0x00038998 File Offset: 0x00036B98
+		internal static Bitmap Software_Dana__Imagens_21
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("Software_Dana (Imagens)21", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x1700000F RID: 15
+		// (get) Token: 0x060001C6 RID: 454 RVA: 0x000389C8 File Offset: 0x00036BC8
+		internal static Bitmap VERSÃO_RESPONSIVA_BRANCA___PNG
+		{
+			get
+			{
+				object obj = Resources.ResourceManager.GetObject("VERSÃO RESPONSIVA BRANCA - PNG", Resources.resourceCulture);
+				return (Bitmap)obj;
+			}
+		}
+
+		// Token: 0x04000332 RID: 818
+		private static ResourceManager resourceMan;
+
+		// Token: 0x04000333 RID: 819
+		private static CultureInfo resourceCulture;
+	}
+}
